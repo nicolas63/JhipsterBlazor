db4bb3fc6a23d03bb8e3f36d3465b82025e45a73
nicolas63 <nicolas_raymond@orange.fr>
2020-06-03 23:25:48 +0200

    Add TU

--- JhipsterBlazor.Test/LoginTest.cs
+++ JhipsterBlazor.Test/LoginTest.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 using Bunit;
 using Bunit.Mocking.JSInterop;
@@ -21,7 +23,7 @@ namespace JhipsterBlazor.Test
     public class LoginTest : TestContext
     {
         [Fact]
-        public void Should_CallSignInMethod_When_LoginFormIdSubmit()
+        public void Should_CallSignInMethod_When_LoginFormIsSubmit()
         {
             // Arrange
             var loginModel = new LoginModel()
@@ -32,18 +34,47 @@ namespace JhipsterBlazor.Test
             var authenticationService = new Mock<MockAuthenticationService>();
             authenticationService.Setup(authenticationService =>
                 authenticationService.SignIn(It.IsAny<LoginModel>()));
-            Services.AddSingleton<AuthenticationStateProvider>(authenticationService.Object); 
+            Services.AddSingleton<AuthenticationStateProvider>(authenticationService.Object);
 
             var login = RenderComponent<Login>();
             login.Instance.LoginModel = loginModel;
 
-            //Act
+            // Act
             login.Find("form").Submit();
 
             // Assert
             authenticationService.Verify(mock => mock.SignIn(It.Is<LoginModel>(model =>
-                model.Username.Equals(loginModel.Username) && model.Password.Equals(loginModel.Password))), Times.Once());
+                    model.Username.Equals(loginModel.Username) && model.Password.Equals(loginModel.Password))),
+                Times.Once());
+        }
 
+        [Fact]
+        public void Should_DisplayFailed_When_CredentialIsInvalid()
+        {
+            // Arrange
+            var loginModel = new LoginModel()
+            {
+                Username = "test",
+                Password = "test"
+            };
+            var authenticationService = new Mock<MockAuthenticationService>();
+            authenticationService.Setup(authenticationService =>
+                authenticationService.SignIn(It.IsAny<LoginModel>())).Returns(Task.FromResult(false));
+            Services.AddSingleton<AuthenticationStateProvider>(authenticationService.Object);
+
+            var login = RenderComponent<Login>();
+            login.Instance.LoginModel = loginModel;
+
+            // Act
+            login.Find("form").Submit();
+
+            // Assert
+            login.Find("div").Children.First().MarkupMatches(
+                @"<div class=""col-md-8"">
+                    <div class=""alert alert-danger"">
+                        <strong>Failed to sign in!</strong> Please check your credentials and try again.
+                    </div>
+                </div>");
         }
     }
 }
