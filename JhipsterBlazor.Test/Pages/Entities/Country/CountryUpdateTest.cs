d7603277e42e45d791526f9273b6fd353a3fba5d
nicolas63 <nicolas_raymond@orange.fr>
2020-09-03 22:02:34 +0200

    Added unit Test on Delete Entity

--- JhipsterBlazor.Test/Pages/Entities/Country/CountryUpdateTest.cs
+++ JhipsterBlazor.Test/Pages/Entities/Country/CountryUpdateTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoFixture;
@@ -8,10 +9,12 @@ using Blazorise;
 using Blazorise.Bootstrap;
 using Blazorise.Icons.FontAwesome;
 using Bunit;
+using Bunit.Rendering;
 using FluentAssertions;
 using JhipsterBlazor.Models;
 using JhipsterBlazor.Services.EntityServices.Country;
 using JhipsterBlazor.Shared;
+using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
@@ -19,13 +22,13 @@ using Xunit;
 
 namespace JhipsterBlazor.Test.Pages.Entities.Country
 {
-    public class CountryTest : TestContext
+    public class CountryUpdateTest : TestContext
     {
-        private readonly Mock<ICountryService> _countryService;
+        /*private readonly Mock<ICountryService> _countryService;
         private readonly Mock<IModalService> _modalService;
         private readonly AutoFixture.Fixture _fixture = new AutoFixture.Fixture();
 
-        public CountryTest()
+        public CountryUpdateTest()
         {
             _countryService = new Mock<ICountryService>();
             _modalService = new Mock<IModalService>();
@@ -80,10 +83,12 @@ namespace JhipsterBlazor.Test.Pages.Entities.Country
             var countries = _fixture.CreateMany<CountryModel>(10);
             _countryService.Setup(service => service.GetAll()).Returns(Task.FromResult(countries.ToList() as IList<CountryModel>));
             //_countryService.Setup(service => service.De()).Returns(Task.FromResult(countries.ToList() as IList<CountryModel>));
+            var modalService = new Mock<IModalService>();
+            Services.AddSingleton(modalService.Object); 
            
             var modalRef = new Mock<IModalReference>();
             modalRef.Setup(mock => mock.Result).Returns(Task.FromResult(ModalResult.Ok(new { })));
-            _modalService.Setup(service => service.Show<DeleteModal>(It.IsAny<string>())).Returns(modalRef.Object);
+            modalService.Setup(service => service.Show<DeleteModal>(It.IsAny<string>())).Returns(modalRef.Object);
             var countryPage = RenderComponent<JhipsterBlazor.Pages.Entities.Country.Country>();
             // Act
             var countryToDelete = countries.First();
@@ -94,7 +99,7 @@ namespace JhipsterBlazor.Test.Pages.Entities.Country
             _countryService.Verify(service => service.Delete(countryToDelete.Id.ToString()), Times.Once);
             var countriesTableBody = countryPage.Find("tbody");
             countriesTableBody.ChildElementCount.Should().Be(9);
-        }
+        }*/
 
     }
 }
